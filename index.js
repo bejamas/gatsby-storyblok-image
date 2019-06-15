20209cc49fbbb46407d964058e8be966b9d68623
Thom Krupa <thom@bejamas.io>
2019-06-15 14:34:49 +0200

    0.1.1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// noop
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+
+var _getFixedGatsbyImage = _interopRequireDefault(require("./getFixedGatsbyImage"));
+
+exports.getFixedGatsbyImage = _getFixedGatsbyImage.default;
+
+var _getFluidGatsbyImage = _interopRequireDefault(require("./getFluidGatsbyImage"));
+
+exports.getFluidGatsbyImage = _getFluidGatsbyImage.default;
