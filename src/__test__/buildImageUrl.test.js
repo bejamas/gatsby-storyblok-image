a89da0c9a2bf2e93b6716d8ec5a5c2c5edbfe60b
ark.gorecki <aaron@bejamas.io>
2019-07-05 18:11:13 +0200

    test buildImageUrl

--- src/__test__/buildImageUrl.test.js
+++ src/__test__/buildImageUrl.test.js
@@ -16,4 +16,16 @@ describe('utils/buildImageUrl.js', function() {
   it('image url does not contain format filter if the same as original image extension', function() {
     assert.notInclude(buildImageUrl(demoOrginalPath, { quality: 80, format: 'jpeg' }), 'format:')
   })
+
+  it("image url contain 'smart' filter", function() {
+    assert.include(buildImageUrl(demoOrginalPath, { quality: 80, format: 'jpeg', smartCrop: true }), '/smart')
+  })
+
+  it("image url contain 'fill' filter", function() {
+    assert.include(buildImageUrl(demoOrginalPath, { quality: 80, format: 'jpeg', fill: 'ccc' }), 'fill:(ccc)')
+  })
+
+  it('image url does not have any filters', function() {
+    assert.notInclude(buildImageUrl(demoOrginalPath, {}), '/filters:')
+  })
 })
