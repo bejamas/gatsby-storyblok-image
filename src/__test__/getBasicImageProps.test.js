0641204c6276ebb207c3a55d6109d2381152c783
ark.gorecki <aaron@bejamas.io>
2019-07-05 17:42:28 +0200

    add test for getBasicImageProps

---
+++ src/__test__/getBasicImageProps.test.js
@@ -0,0 +1,44 @@
+import { assert } from 'chai'
+
+import getBasicImageProps, { validImageUrlPattern } from '../utils/getBasicImageProps'
+
+const demoImageUrl = '//a.storyblok.com/f/39898/3310x2192/e4ec08624e/demo-image.jpeg'
+
+describe('utils/getBasicImageProps.js', function() {
+  describe('check valid image URL', function() {
+    it('should return true with passed url', function() {
+      const testURL = demoImageUrl
+      assert.match(testURL, validImageUrlPattern)
+    })
+
+    it('should return false with passed url (wrong image dimensions)', function() {
+      const testURL = '//a.storyblok.com/f/39898/33102192/e4ec08624e/demo-image.jpeg'
+      assert.notMatch(testURL, validImageUrlPattern)
+    })
+
+    it('should return false with passed url (no random string before image name)', function() {
+      const testURL = '//a.storyblok.com/f/39898/3310x2192/demo-image.jpeg'
+      assert.notMatch(testURL, validImageUrlPattern)
+    })
+
+    it('should return false with passed url (whitespaces in image name)', function() {
+      const testURL = '//a.storyblok.com/f/39898/3310x2192/e4ec08624e/demo image.jpeg'
+      assert.notMatch(testURL, validImageUrlPattern)
+    })
+  })
+
+  describe('getBasicImageProps()', function() {
+    const demoImageProperty = getBasicImageProps(demoImageUrl)
+    it('passed valid image url return object with metadata property', function() {
+      assert.property(demoImageProperty, 'metadata')
+    })
+
+    it('passed valid image url return object with originalPath property', function() {
+      assert.exists(demoImageProperty, demoImageProperty.originalPath)
+    })
+
+    it('passed valid image url return object with originalPath property', function() {
+      assert.exists(demoImageProperty, demoImageProperty.extension)
+    })
+  })
+})
