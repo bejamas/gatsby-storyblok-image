5510d1fbf82c051e9274eee169028a6279e789b6
ark.gorecki <aaron@bejamas.io>
2019-07-05 17:06:14 +0200

    refactor isWebP tests

--- src/__test__/helpers.test.js
+++ src/__test__/helpers.test.js
@@ -1,26 +1,28 @@
 import assert from 'assert'
 import { isWebP } from '../utils/helpers'
 
-const urlWithNoWebPFormat = "https://img2.storyblok.com/f/59501/946x423/ea63afe874/bitmap.png"
-const urlWithWebPFormat = "https://img2.storyblok.com/filters:quality(100):format(webp)/f/59501/946x423/ea63afe874/bitmap.png"
-const urlWebPImage = "https://img2.storyblok.com/f/59501/946x423/ea63afe874/bitmap.webp"
+describe('utils/helpers.js', function() {
+  describe('isWebP()', function() {
+    const urlWithNoWebPFormat = 'https://img2.storyblok.com/f/59501/946x423/ea63afe874/bitmap.png'
+    const urlWithWebPFormat =
+      'https://img2.storyblok.com/filters:quality(100):format(webp)/f/59501/946x423/ea63afe874/bitmap.png'
+    const urlWebPImage = 'https://img2.storyblok.com/f/59501/946x423/ea63afe874/bitmap.webp'
 
-describe('helpers', () => {
-  describe('isWebP', () => {
+
     it('should return false when the url is not passed', function() {
-      assert.equal(isWebP(), false);
-    });
+      assert.equal(isWebP(), false)
+    })
 
     it('should return false when the url is not passed with "filters:format(webp)"', function() {
-      assert.equal(isWebP(urlWithNoWebPFormat), false);
-    });
+      assert.equal(isWebP(urlWithNoWebPFormat), false)
+    })
 
     it('should return true when the url is passed with ":format(webp)"', function() {
-      assert.equal(isWebP(urlWithWebPFormat), true);
-    });
-  
+      assert.equal(isWebP(urlWithWebPFormat), true)
+    })
+
     it('should return true when the url is webp image', function() {
-      assert.equal(isWebP(urlWebPImage), true);
-    });
+      assert.equal(isWebP(urlWebPImage), true)
+    })
   })
 })
