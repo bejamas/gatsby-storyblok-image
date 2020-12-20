dd0bdb113c854b1941e083704056c67afda5ea56
Arek Gorecki <ark.gorecki@gmail.com>
2020-12-20 12:36:14 +0100

    adds support for svg urls in validImageUrlPattern regex
    svg's comes with "empty" dimension parts, which past regex qualified as not allowed src

--- src/utils/getBasicImageProps.js
+++ src/utils/getBasicImageProps.js
@@ -1,4 +1,4 @@
-const validImageUrlPattern = /^(https?:)?\/\/a.storyblok.com\/f\/[0-9]+\/[0-9]+x[0-9]+\/[A-Za-z0-9]+\/[\S]+\.[a-z]+/
+const validImageUrlPattern = /^(https?:)?\/\/a.storyblok.com\/f\/[0-9]+\/[0-9]*x[0-9]*\/[A-Za-z0-9]+\/[\S]+\.[a-z]+/
 
 function getBasicImageProps(image) {
   let url = null
